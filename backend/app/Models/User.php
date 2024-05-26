41f71dd06278cce66743a9faae1782dc46b43097
Jaciesune <lutasek9@gmail.com>
2024-05-26 19:08:00 +0200

    Fix in dockerfiles to load

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Tymon\JWTAuth\Contracts\JWTSubject;
+use App\Models\logs;
 
 class User extends Authenticatable implements JWTSubject, MustVerifyEmail
 {
@@ -69,5 +70,10 @@ class User extends Authenticatable implements JWTSubject, MustVerifyEmail
     public function accounts()
     {
         return $this->belongsToMany(Account::class);
+    }
+
+    public function logs()
+    {
+        return $this->hasMany(logs::class);
     }
 }
