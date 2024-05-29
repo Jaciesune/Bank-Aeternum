243cfe6f73e24d4f27dedbeff45a9690c35cf55c
Jaciesune <lutasek9@gmail.com>
2024-05-29 18:53:18 +0200

    Changed Notification List On Dashboard To RAW Querry

--- backend/app/Http/Controllers/Notification/NotificationController.php
+++ backend/app/Http/Controllers/Notification/NotificationController.php
@@ -4,26 +4,17 @@ namespace App\Http\Controllers\Notification;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Notifications\NotificationResource;
-use App\Models\Notification;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Notifications\DatabaseNotification;
+use Illuminate\Support\Facades\DB;
 
 class NotificationController extends Controller
 {
     public function index()
     {
-        $notifications = Auth::user()->notifications;
-        return NotificationResource::collection($notifications);
-    }
+        $userId = Auth::id();
+        $notifications = DB::select('SELECT id, user_id, title, content, created_at FROM notifications WHERE user_id = ?', [$userId]);
 
-    // public function markAsRead($id)
-    // {
-    //     $notification = Auth::user()->notifications->where('id', $id)->first();
-    //     if ($notification) {
-    //         $notification->markAsRead();
-    //         return response()->json(['message' => 'Notification marked as read'], 200);
-    //     }
-    //     return response()->json(['message' => 'Notification not found'], 404);
-    // }
+        $notificationsCollection = collect($notifications);
+        return NotificationResource::collection($notificationsCollection);
+    }
 }
